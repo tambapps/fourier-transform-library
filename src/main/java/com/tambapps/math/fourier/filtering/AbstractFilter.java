be8ddc04f8f0655f747344b1c7bee1f8701e1af9
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-10-23 16:26:53 +0200

    fixed error

--- src/main/java/com/tambapps/math/fourier/filtering/AbstractFilter.java
+++ src/main/java/com/tambapps/math/fourier/filtering/AbstractFilter.java
@@ -3,7 +3,7 @@ package com.tambapps.math.fourier.filtering;
 import com.tambapps.math.carray2d.CArray2D;
 import com.tambapps.math.complex.Complex;
 
-abstract class AbstractFilter implements Filter {
+public abstract class AbstractFilter implements Filter {
 
   @Override
   public final void apply(CArray2D array) {
--- src/main/java/com/tambapps/math/fourier/filtering/Filters.java
+++ src/main/java/com/tambapps/math/fourier/filtering/Filters.java
@@ -8,15 +8,15 @@ public final class Filters {
   private Filters() {
   }
 
-  public static Filter rectangle(int width, int height, boolean inverted) {
-    return new RectangleFilter(width, height, inverted);
+  public static Filter rectangle(int width, int height, boolean filterIn) {
+    return new RectangleFilter(width, height, filterIn);
   }
 
-  public static Filter threshold(double max) {
-    return new ThresholdFilter(max);
+  public static Filter threshold(double max, boolean filterLower) {
+    return new ThresholdFilter(max, filterLower);
   }
 
-  public static Filter circle(int radius, boolean reverted) {
-    return new CircleFilter(radius, reverted);
+  public static Filter circle(int radius, boolean filterIn) {
+    return new CircleFilter(radius, filterIn);
   }
 }
