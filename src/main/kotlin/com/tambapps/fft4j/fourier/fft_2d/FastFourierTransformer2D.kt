f0775cc0486ca2e2bb041de8ec48b3935bf4a268
Nelson <nfonkoua@weborama.com>
2020-07-27 15:11:19 +0200

    implemented fft 2d

--- src/main/kotlin/com/tambapps/fft4j/fourier/fft_2d/FastFourierTransformer2D.kt
+++ src/main/kotlin/com/tambapps/fft4j/fourier/fft_2d/FastFourierTransformer2D.kt
@@ -13,58 +13,34 @@ import java.util.concurrent.Future
 
 class FastFourierTransformer2D(private val executor: ExecutorService, var chooser: AlgorithmChooser) {
 
-    // TODO use kotlin types instead of interface
-    interface TaskSupplier {
-        fun supply(f: CArray2D, i: Int): Runnable
-    }
-
     @Throws(FourierException::class)
     fun transform(f: CArray2D) {
         val algorithm = chooser.getAlgorithm(f.m, f.n)
-        compute(f,
-                object : TaskSupplier {
-                    override fun supply(f: CArray2D, i: Int): Runnable {
-                        return FourierTransformRowTask(algorithm, f, i)
-                    }
-                },
-                object : TaskSupplier {
-                    override fun supply(f: CArray2D, i: Int): Runnable {
-                        return FourierTransformColumnTask(algorithm, f, i)
-                    }
-                }
-        )
+        compute(f, {  i -> FourierTransformRowTask(algorithm, f, i) },
+                { i -> FourierTransformColumnTask(algorithm, f, i) })
     }
 
     @Throws(FourierException::class)
     fun inverse(f: CArray2D) {
         val algorithm = chooser.getAlgorithm(f.m, f.n)
-        compute(f,
-                object : TaskSupplier {
-                    override fun supply(f: CArray2D, i: Int): Runnable {
-                        return FourierInverseRowTask(algorithm, f, i)
-                    }
-                },
-                object : TaskSupplier {
-                    override fun supply(f: CArray2D, i: Int): Runnable {
-                        return FourierInverseColumnTask(algorithm, f, i)
-                    }
-                }
-        )
+        compute(f, {  i -> FourierInverseRowTask(algorithm, f, i) },
+                { i -> FourierInverseColumnTask(algorithm, f, i) })
     }
 
     @Throws(FourierException::class)
-    private fun compute(f: CArray2D, rowTaskSupplier: TaskSupplier, columnTaskSupplier: TaskSupplier) {
+    private fun compute(f: CArray2D, rowTaskSupplier: (Int) -> Runnable, columnTaskSupplier: (Int) -> Runnable) {
         val futures = mutableListOf<Future<*>>()
         for (i in 0 until f.m) {
-            futures.add(executor.submit(rowTaskSupplier.supply(f, i)))
+            futures.add(executor.submit(rowTaskSupplier(i)))
         }
         waitFutures(futures)
         for (i in 0 until f.n) {
-            futures.add(executor.submit(columnTaskSupplier.supply(f, i)))
+            futures.add(executor.submit(columnTaskSupplier(i)))
         }
         waitFutures(futures)
     }
 
+    @Throws(FourierException::class)
     private fun waitFutures(futures: MutableList<Future<*>>) {
         futures.removeIf {
             try {
