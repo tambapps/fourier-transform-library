7aed0891aa5626a5316ffe4d2376f84f0acfc452
Nelson Fonkoua <tambue@hotmail.fr>
2022-12-03 23:53:54 +0100

    new test

--- src/test/java/com/tambapps/fft4j/fourier/fft_2d/FastFourierTransformer2DTest.java
+++ src/test/java/com/tambapps/fft4j/fourier/fft_2d/FastFourierTransformer2DTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 
 import com.tambapps.fft4j.complex.array2d.CArray2D;
 import com.tambapps.fft4j.complex.Complex;
+import com.tambapps.fft4j.complex.vector.CVector;
+import com.tambapps.fft4j.complex.vector.ImmutableCVector;
 import org.junit.Test;
 
 import java.util.Random;
@@ -34,4 +36,36 @@ public class FastFourierTransformer2DTest {
       assertEquals("Should be equal",originArray.get(i), array2D.get(i));
     }
   }
+
+  @Test
+  public void test() {
+    Complex ONE = Complex.of(1);
+    final Complex[] input = new Complex[] {
+        ONE,
+        ONE,
+        ONE,
+        ONE,
+        Complex.ZERO,
+        Complex.ZERO,
+        Complex.ZERO,
+        Complex.ZERO
+    };
+
+    CArray2D array2D = new CArray2D(input.length, 1, input);
+
+    assertTrue("Should be true", transformer2D.transform(array2D));
+
+
+    final CVector expected = ImmutableCVector.of(Complex.of(4),
+        Complex.of(1d, -2.414214),
+        Complex.ZERO,
+        Complex.of(1, -0.414214),
+        Complex.ZERO,
+        Complex.of(1, 0.414214),
+        Complex.ZERO,
+        Complex.of(1, 2.414214));
+
+    assertEquals("Should be equal", expected, array2D.getColumn(0));
+  }
+
 }
